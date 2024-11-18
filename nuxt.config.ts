3a35edbc820655207d2c7251a96c02a2485ef3d0
zhougonglai <838048635@qq.com>
2024-11-18 15:42:17 +0800

    init project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  css: ['~/assets/style/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
   routeRules: {
     // prerender index route by default
     '/': { prerender: true },
